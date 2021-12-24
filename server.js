88c4650ae3f6fee4c8f5dfdb4135050ed1e16dcc
massinissa06 <massinfoo@gmail.com>
2021-12-24 15:42:48 +0100

    update password mongodb

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb+srv://Massi:<17ZSKCUg3CYS4nVU>@cluster0.4djrr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Massi:17ZSKCUg3CYS4nVU@cluster0.4djrr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
